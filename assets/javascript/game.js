b005690a2fe0912219b5b3e96626ff06b1357c27
Kennan <kennan.smith1@gmail.com>
2018-04-24 17:50:51 -0600

    good start on javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,92 @@
 
 var words = ["skydive", "snowboard", "skateboard", "longboard", "surf", "raft", "wakeboard", "luge", "bmx", "climbing"];
 var word;
-var wordArr = word.split("");
-var wordLen = wordArr.length;
-var wrong = [];
-var wins = 0;
-var remG = 7;
+var wordArr;
+var wordLen;
+var remG;
+var guesses;
+var wins;
+
 
 // store list of vars for quick reference
 // var words = ["skydive", "snowboard", "skateboard", "longboard", "surf", "raft", "wakeboard", "luge", "bmx", "climbing"];
 // var word;
-// var wordArr = word.split("");
-// var wordLen = wordArr.length;
-// var wrong = [];
-// var wins = 0;
-// var remG = 7;
+// var wordArr;
+// var wordLen;
+// var remG;
+// var guesses;
+// var wins;
 
 
 // functions
 
 // attach to a New Game button to start a new game
 function newGame() {
+    // reset number of remaining guesses
+    var remG = 6;
+
+    // choose random word from words array
+    var word = words[Math.floor(Math.random() * words.length)];
+    console.log("word: " + word);
+
+    // split word into array of individual letters
+    var wordArr = word.split("");
+
+    // create uArr
+    var uArr = [];
+    for (var i=0; i<wordArr.length; i++) {
+        uArr.push("_");
+    }
+    console.log("uArr: " + uArr);
+
+    // create array for previous guesses
+    var guesses = [];
+
+    // store important info from current/prev game
+    // 
+    // 
+    // 
+    
+    // write to html to display uArr, fresh noose, etc.
+    // 
+    // 
+    // 
 
 }
 
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
-    var word = words[Math.floor(Math.random() * words.length)];
+
+    // add userGuess to guesses array
+    guesses.push(userGuess);
+
+    // check if userGuess is a correct guess
+    if (wordArr.indexOf(userGuess) > -1) {
+        // if correct, replace it with corresponding _ in uArr
+        uArr[wordArr.indexOf(userGuess)] = userGuess;
+        console.log("current uArr: " + uArr);
+        // check if word is complete (user wins)
+        if (uArr.indexOf("_") == "undefined") {
+            // USER WINS - tell them to hit New Game button to start over
+            // 
+            // 
+            // 
+        }
+    } else {
+        // if incorrect, subtract 1 from remG, add body part
+        remG--;
+        console.log("current remG: " + remG);
+        // code to add body part
+        // 
+        // 
+        // 
+        if (remG = 0) {
+            // USER LOSES - tell them to hit New Game button to start over
+            // 
+            // 
+            // 
+        }
+    }
+
 }
